9ef9b92b1e3f371d7df4540999567886c354197c
Vilim Krajinovic <vkrajinovic@IB-VKRAJINOVIC-M.local>
2021-02-06 13:09:15 +0100

    Initial commit

---
+++ src/main/java/com/crypto/org/cryptonotifier/CryptoNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.org.cryptonotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoNotifierApplication.class, args);
+	}
+
+}
