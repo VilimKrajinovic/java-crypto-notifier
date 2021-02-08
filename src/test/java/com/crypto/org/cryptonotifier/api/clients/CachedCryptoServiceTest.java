f1717fb18889b0e2b263d8e6581a07ca2197f86e
Vilim Krajinovic <vkrajinovic@IB-VKRAJINOVIC-M.local>
2021-02-08 12:13:49 +0100

    Upgraded to Junit5

--- src/test/java/com/crypto/org/cryptonotifier/api/clients/CachedCryptoServiceTest.java
+++ src/test/java/com/crypto/org/cryptonotifier/api/clients/CachedCryptoServiceTest.java
@@ -4,11 +4,11 @@ import com.crypto.org.cryptonotifier.api.models.CryptoCurrencyInfo;
 import com.crypto.org.cryptonotifier.api.service.CachedCryptoService;
 import com.crypto.org.cryptonotifier.api.service.CryptoService;
 import com.google.common.cache.Cache;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class CachedCryptoServiceTest {
 
     private static final CryptoCurrencyInfo CRYPTO_INFO = new CryptoCurrencyInfo();
@@ -30,8 +30,8 @@ public class CachedCryptoServiceTest {
 
     CachedCryptoService cachedCryptoService;
 
-    @Before
-    public void setup(){
+    @BeforeEach
+    void init(){
         given(cryptosCache.asMap()).willReturn(new ConcurrentHashMap<>());
         cachedCryptoService = new CachedCryptoService(delegateService, cryptosCache);
     }
